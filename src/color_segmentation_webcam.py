426230e2d5721ca436aecc53294245ca19308804
arvin_febriyan <arvin_febriyan@kelaberetiv.sg>
2020-05-27 20:22:34 +0800

    added segmentation with webcam

---
+++ src/color_segmentation_webcam.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+import numpy as np
+
+# Creating a VideoCapture object
+cap = cv2.VideoCapture(0)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+# Delay to wait for camera
+time.sleep(3)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Set threshold
+    lower_green = (25, 50, 25)
+    upper_green = (120, 255, 255)
+
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("frame", result)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
