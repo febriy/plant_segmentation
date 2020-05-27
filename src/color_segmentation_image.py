be24fbdb2c74c21ce19f56ce35cd2eb64de66d4f
arvin_febriyan <arvin_febriyan@kelaberetiv.sg>
2020-05-27 20:36:52 +0800

    calculate area using contour

--- src/color_segmentation_image.py
+++ src/color_segmentation_image.py
@@ -19,10 +19,31 @@ def open_image(image_dir):
 lower_green = (25, 50, 25)
 upper_green = (120, 255, 255)
 
+
+def find_mask_contour_area(mask):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    # cv2.imshow("opening", opening)
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    area = 0
+    for c in cnts:
+        area += cv2.contourArea(c)
+        cv2.drawContours(plant, [c], 0, (0, 0, 0), 2)
+
+    return area
+
+
 if __name__ == "__main__":
-    hsv_plant, plant = open_image("data/ara2013_plant160_rgb.png")
+    hsv_plant, plant = open_image("data/lettuce.jpeg")
     mask = cv2.inRange(hsv_plant, lower_green, upper_green)
     result = cv2.bitwise_and(plant, plant, mask=mask)
+
+    # use contours to find mask area
+    # https://stackoverflow.com/questions/57282935/how-to-detect-area-of-pixels-with-the-same-color-using-opencv
+    area = find_mask_contour_area(mask)
+    print(area)
 
     plt.subplot(1, 2, 1)
     plt.imshow(mask, cmap="gray")
